ca546dbce3771bc51050c9170bc467725b712984
Abdellah <abdellahbounab@gmail.com>
2024-03-15 10:26:29 +0000

    adding my road map first

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int av, char **ac)
+{
+    int fd[2];
+
+    if (av == 5)
+    {
+        \\understanding the concept of pipe
+        \\checking on errors that can occurs
+        \\handle the errors
+        \\start coding journey
+        \\debugging
+        \\checking and testing
+        \\few more learnings
+        \\applying the project
+    }
+}
